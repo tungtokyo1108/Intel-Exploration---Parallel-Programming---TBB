88383e90351164c33ae2872bd85b76d24dbb66bd
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-09-25 14:38:43 +0900

    Add files via upload

---
+++ IntelParallel_Start.cpp
@@ -0,0 +1,17 @@
+/*
+ * IntelParallel_Start.cpp
+ *
+ *  Created on: Sep 25, 2018
+ *      Author: Tung1108
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Let's to study!!!" << endl;
+	return 0;
+}
+
+
