4521ee43b82d9e027fbc448bf51d6af23bd73da5
Stand on the shoulders of giants <dangthanhtung91@gmail.com>
2018-10-07 00:51:13 +0900

    Update tbb_machine.h

--- include/tbb/tbb_machine.h
+++ include/tbb/tbb_machine.h
@@ -572,7 +572,205 @@ inline void __TBB_store_relaxed(volatile T& location, V value) {
   machine_load_store_relaxed<T,sizeof(T)>::store(const_cast<T&>(location), T(value));
 }  
   
+#ifndef __TBB_TypeWithAlignmentAtLeastAsStrict
+#if __TBB_ALIGNAS_PRESENT
+#define __TBB_DefineTypeWithAlignment(PowerOf2) \
+struct alignas(PowerOf2) __TBB_machine_type_with_alignment_##PowerOf2 { \
+  uint32_t member[PowerOf2/sizeof(uint32_t)];                           \
+};
+#define __TBB_alignof(T) alignof(T)
+#elif __TBB_ATTRIBUTE_ALIGNED_PRESENT
+
+#define __TBB_DefineTypeWithAlignment(PowerOf2) \
+struct __TBB_machine_type_with_alignment_##PowerOf2 { \
+  uint32_t member[PowerOf2/sizeof(uint32_t)];         \
+} __attribute__((aligned(PowerOf2)));
+#define __TBB_alignof(T) __alignof__(T)
+
+#elif __TBB_DECLSPEC_ALIGN_PRESENT
+#define __TBB_DefineTypeWithAlignment(PowerOf2) \
+__declspec(align(PowerOf2))                     \
+struct __TBB_machine_type_with_alignment_##PowerOf2 { \
+  uint32_t member[PowerOf2/sizeof(uint32_t)];         \
+};
+#define __TBB_alignof(T) __alignof(T)
+#else
+#error
+#endif
+
+__TBB_DefineTypeWithAlignment(16)
+__TBB_DefineTypeWithAlignment(32)
+__TBB_DefineTypeWithAlignment(64)
+
+typedef __TBB_machine_type_with_alignment_64 __TBB_machine_type_with_strictest_alignment;
+template<size_t N> struct type_with_alignment;
+
+template<> struct type_with_alignment<1> {char member;};
+template<> struct type_with_alignment<2> {uint16_t member;};
+template<> struct type_with_alignment<4> {uint32_t member;};
+template<> struct type_with_alignment<8> {uint64_t member;};
+template<> struct type_with_alignment<16> {__TBB_machine_type_with_alignment_16 member;};
+template<> struct type_with_alignment<32> {__TBB_machine_type_with_alignment_32 member;};
+template<> struct type_with_alignment<64> {__TBB_machine_type_with_alignment_64 member;};
+
+#if __TBB_ALIGNOF_NOT_INSTANTIATED_TYPES_BROKEN
+template<size_t Size, typename T>
+struct work_around_alignment_bug {
+  static const size_t alignment = __TBB_alignof(T);
+}:
+#define __TBB_TypeWithAlignmentAtLeastAsStrict(T) tbb::internal::type_with_alignment<tbb::internal::work_around_alignment_bug<sizeof(T),T>::alignment>
+#else
+#define __TBB_TypeWithAlignmentAtLeastAsStrict(T) tbb::internal::type_with_alignment<__TBB_alignof(T)>
+#endif
+#endif
+
+template<typename T>
+struct reverse {
+  static const T byte_table[256];
+};
+
+/* An efficient implementation of the reverse function utilizes a 2^8 lookup table
+   holding the bit-reversed values
+*/
+template<typename T>
+const T reverse<T>::byte_table[256] = {
+    0x00, 0x80, 0x40, 0xC0, 0x20, 0xA0, 0x60, 0xE0, 0x10, 0x90, 0x50, 0xD0, 0x30, 0xB0, 0x70, 0xF0,
+    0x08, 0x88, 0x48, 0xC8, 0x28, 0xA8, 0x68, 0xE8, 0x18, 0x98, 0x58, 0xD8, 0x38, 0xB8, 0x78, 0xF8,
+    0x04, 0x84, 0x44, 0xC4, 0x24, 0xA4, 0x64, 0xE4, 0x14, 0x94, 0x54, 0xD4, 0x34, 0xB4, 0x74, 0xF4,
+    0x0C, 0x8C, 0x4C, 0xCC, 0x2C, 0xAC, 0x6C, 0xEC, 0x1C, 0x9C, 0x5C, 0xDC, 0x3C, 0xBC, 0x7C, 0xFC,
+    0x02, 0x82, 0x42, 0xC2, 0x22, 0xA2, 0x62, 0xE2, 0x12, 0x92, 0x52, 0xD2, 0x32, 0xB2, 0x72, 0xF2,
+    0x0A, 0x8A, 0x4A, 0xCA, 0x2A, 0xAA, 0x6A, 0xEA, 0x1A, 0x9A, 0x5A, 0xDA, 0x3A, 0xBA, 0x7A, 0xFA,
+    0x06, 0x86, 0x46, 0xC6, 0x26, 0xA6, 0x66, 0xE6, 0x16, 0x96, 0x56, 0xD6, 0x36, 0xB6, 0x76, 0xF6,
+    0x0E, 0x8E, 0x4E, 0xCE, 0x2E, 0xAE, 0x6E, 0xEE, 0x1E, 0x9E, 0x5E, 0xDE, 0x3E, 0xBE, 0x7E, 0xFE,
+    0x01, 0x81, 0x41, 0xC1, 0x21, 0xA1, 0x61, 0xE1, 0x11, 0x91, 0x51, 0xD1, 0x31, 0xB1, 0x71, 0xF1,
+    0x09, 0x89, 0x49, 0xC9, 0x29, 0xA9, 0x69, 0xE9, 0x19, 0x99, 0x59, 0xD9, 0x39, 0xB9, 0x79, 0xF9,
+    0x05, 0x85, 0x45, 0xC5, 0x25, 0xA5, 0x65, 0xE5, 0x15, 0x95, 0x55, 0xD5, 0x35, 0xB5, 0x75, 0xF5,
+    0x0D, 0x8D, 0x4D, 0xCD, 0x2D, 0xAD, 0x6D, 0xED, 0x1D, 0x9D, 0x5D, 0xDD, 0x3D, 0xBD, 0x7D, 0xFD,
+    0x03, 0x83, 0x43, 0xC3, 0x23, 0xA3, 0x63, 0xE3, 0x13, 0x93, 0x53, 0xD3, 0x33, 0xB3, 0x73, 0xF3,
+    0x0B, 0x8B, 0x4B, 0xCB, 0x2B, 0xAB, 0x6B, 0xEB, 0x1B, 0x9B, 0x5B, 0xDB, 0x3B, 0xBB, 0x7B, 0xFB,
+    0x07, 0x87, 0x47, 0xC7, 0x27, 0xA7, 0x67, 0xE7, 0x17, 0x97, 0x57, 0xD7, 0x37, 0xB7, 0x77, 0xF7,
+    0x0F, 0x8F, 0x4F, 0xCF, 0x2F, 0xAF, 0x6F, 0xEF, 0x1F, 0x9F, 0x5F, 0xDF, 0x3F, 0xBF, 0x7F, 0xFF
+};
 }
+}
+
+using tbb::internal::__TBB_load_with_acquire;
+using tbb::internal::__TBB_store_with_release;
+
+// Mapping historically used name to the expected by atomic_load_stored_traits
+#define __TBB_load_acquire __TBB_load_with_acquire
+#define __TBB_store_release __TBB_store_with_release
+
+#ifndef __TBB_Log2
+inline intptr_t __TBB_Log2(uintptr_t x) {
+  if(x==0) return -1;
+  intptr_t result = 0;
+  #if !defined(_M_ARM)
+  uintptr_t tmp_;
+  if(sizeof(x)>4 && (tmp_ = ((uint64_t)x)>32)) {x=tmp_; result += 32;}
+  #endif
+  if (uintptr_t tmp = x>>16) {x=tmp; result += 16;}
+  if (uintptr_t tmp = x>>8) {x=tmp; result += 8;}
+  if (uintptr_t tmp = x>>4) {x=tmp; result += 4;}
+  if (uintptr_t tmp = x>>2) {x=tmp; result += 2;}
+
+  return (x&2)? result+1 : result;
+}
+#endif
+
+#ifndef __TBB_AtomicOR
+inline void __TBB_AtomicOR(volatile void *operand, uintptr_t addend) {
+  for (tbb::internal::atomic_backoff b;;b.pause()) {
+    uintptr_t tmp = *(volatile uintptr_t *)operand;
+    uintptr_t result = __TBB_CompareAndSwapW(operand, tmp|addend, tmp);
+    if (result == tmp) break;
+  }
+}
+#endif
+
+#ifndef __TBB_AtomicAND
+inline void __TBB_AtomicAND(volatile void *operand, uintptr_t addend) {
+  for (tbb::internal::atomic_backoff b;;b.pause()) {
+    uintptr_t tmp = *(volatile uintptr_t *)operand;
+    uintptr_t result = __TBB_CompareAndSwapW(operand, tmp&addend, tmp);
+    if (result == tmp) break;
+  }
+}
+#endif
+
+#if __TBB_PREFETCHING
+
+#ifndef __TBB_cl_prefetch
+#error This platform does not define cache management primitives required for __TBB_PREFETCHING
+#endif
+
+#ifndef __TBB_cl_evict
+#define __TBB_cl_evict(p)
+#endif
+#endif
+
+#ifndef __TBB_Flag
+typedef unsigned char __TBB_Flag;
+#endif
+typedef __TBB_atomic __TBB_Flag __TBB_atomic_flag;
+
+#ifndef __TBB_TryLockByte
+inline bool __TBB_TryLockByte(__TBB_atomic_flag &flag) {
+  return __TBB_machine_cmpswp1(&flag,1,0)==0;
+}
+#endif
+
+#ifndef __TBB_LockByte
+inline __TBB_Flag __TBB_LockByte(__TBB_atomic_flag &flag) {
+  tbb::internal::atomic_backoff backoff;
+  while (!__TBB_TryLockByte(flag)) backoff.pause();
+  return 0;
+}
+#endif
+
+#ifndef __TBB_UnlockByte
+#define __TBB_UnlockByte(addr) __TBB_store_with_release((addr),0)
+#endif
+
+#if (__TBB_x86_32 || __TBB_x86_64)
+inline void __TBB_TryLockByteElidedCancel() { __TBB_machine_try_lock_elided_cancel();}
+inline bool __TBB_TryLockByteElided(__TBB_atomic_flag& flag) {
+  bool res = __TBB_machine_try_lock_elided(&flag)!=0;
+  if(!res) __TBB_TryLockByteElidedCancel();
+  return res;
+}
+
+inline void __TBB_TryLockByteElided(__TBB_atomic_flag& flag) {
+  for (;;) {
+    tbb::internal::spin_wait_while_eq(flag,1);
+    if (__TBB_machine_try_lock_elided(&flag))
+    return;
+    __TBB_TryLockByteElidedCancel();
+  }
+}
+
+inline void __TBB_UnlockByteElided(__TBB_atomic_flag& flag) {
+  __TBB_machine_unlock_elided(&flag);
+}
+#endif
+
+#ifndef __TBB_ReverseByte
+inline unsigned char __TBB_ReverseByte(unsigned char src) {
+  return tbb::internal::reverse<unsigned char>::byte_table[src];
+}
+#endif
+
+template<typename T>
+T __TBB_ReverseBits(T src) {
+  T dst;
+  unsigned char *original = (unsigned char *)&src;
+  unsigned char *reversed = (unsigned char *)&dst;
+
+  for (int i = sizeof(T)-1; i>=0; i--)
+  {
+    reversed[i] = __TBB_ReverseByte(original[sizeof(T)-i-1]);
+  }
+  return dst;
 }
 
 #endif /* INCLUDE_TBB_TBB_MACHINE_H_ */
